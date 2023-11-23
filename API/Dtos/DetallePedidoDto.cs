651c88a4c38456e95ee218ce6dcc8a6b02b873ec
CristianReyes2023 <137292974+CristianReyes2023@users.noreply.github.com>
2023-11-23 11:33:53 -0500

    feat: :sparkles: Dtos finished

---
+++ API/Dtos/DetallePedidoDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos;
+public class DetallePedidoDto
+{
+    public int IdPedidoFk { get; set; }
+
+    public string IdProductoFk { get; set; } = null!;
+
+    public int Cantidad { get; set; }
+
+    public decimal PrecioUnidad { get; set; }
+
+    public short NumeroLinea { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using API.Extensions;
+using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
 
@@ -9,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
+builder.Services.AddApplicationServices();
+builder.Services.ConfigureRateLimiting();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 
 builder.Services.AddDbContext<JardineriaContext>(optionsBuilder => // 2611
 {
@@ -42,6 +49,8 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+app.UseCors("CorsPolicy");
+app.UseIpRateLimiting();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
