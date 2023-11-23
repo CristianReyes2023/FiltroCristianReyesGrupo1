61a40f2d02d15e3136d4483269f16811fda07601
CristianReyes2023 <137292974+CristianReyes2023@users.noreply.github.com>
2023-11-23 10:24:14 -0500

    feat: :tada: Structure for 4layerProject and Scaffold in progress

---
+++ Persistence/Entities/DetallePedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.Entities;
+
+public partial class DetallePedido
+{
+    public int IdPedidoFk { get; set; }
+
+    public string IdProductoFk { get; set; } = null!;
+
+    public int Cantidad { get; set; }
+
+    public decimal PrecioUnidad { get; set; }
+
+    public short NumeroLinea { get; set; }
+
+    public virtual Pedido IdPedidoFkNavigation { get; set; } = null!;
+
+    public virtual Producto IdProductoFkNavigation { get; set; } = null!;
+}
---
+++ Persistence/Entities/Pago.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.Entities;
+
+public partial class Pago
+{
+    public int IdClienteFk { get; set; }
+
+    public string FormaPago { get; set; } = null!;
+
+    public string Id { get; set; } = null!;
+
+    public DateOnly FechaPago { get; set; }
+
+    public decimal Total { get; set; }
+
+    public virtual Cliente IdClienteFkNavigation { get; set; } = null!;
+}
